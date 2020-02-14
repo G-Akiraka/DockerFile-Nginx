b968ca4eefdac968d6cac0259a7a464adb7d3c29
Akiraka <>
2020-02-14 13:40:43 +0800

    解决无法启动

--- script/aka_nginx.sh
+++ script/aka_nginx.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-#   启动 crontab 服务
-service cron start
-#   启动 rsyslog 服务
-service rsyslog start
+#   启动 crond 与 rsyslog 服务
+crond && rsyslogd
+
 #   启动 nginx
 /bin/sh -c 'nginx -g "daemon off;"'
